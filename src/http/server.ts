3879c0bbd1d2d5fb0c3b7902af9c44b33091ca79
Diego Cardoso <90847079+diegoCBorba@users.noreply.github.com>
2024-02-07 18:53:14 -0300

    Fim da seção 1

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,29 @@
-import fastify from "fastify";
+import fastify from "fastify"
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod';
 
 const app = fastify()
 
+// conexão com o banco
+const prisma = new PrismaClient()
+
 // GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS
 
-app.post('/polls', (resquest) => {
-    console.log(resquest.body)
-    return 'Hello NLW'
+app.post('/polls', async (resquest, reply) => {
+    const createPollBody = z.object({
+        title: z.string()
+    })
+
+    const { title } = createPollBody.parse(resquest.body)
+
+    const poll = await prisma.poll.create({
+        data: {
+            title,
+        }
+    })
+
+    // Código de status de respostas HTTP utilizando o reply, 201 - criação bem sucedida
+    return reply.status(201).send({ pollId: poll.id })
 })
 
 app.listen({ port: 3333}).then(() => {
