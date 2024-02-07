2577bc5da060c8d12803b7e899cd277b3a600e2e
Diego Cardoso <90847079+diegoCBorba@users.noreply.github.com>
2024-02-06 23:19:03 -0300

    Continuação projeto

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,10 +2,18 @@ import fastify from "fastify";
 
 const app = fastify()
 
-app.get('/hello', () => {
+// GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS
+
+app.post('/polls', (resquest) => {
+    console.log(resquest.body)
     return 'Hello NLW'
 })
 
 app.listen({ port: 3333}).then(() => {
     console.log("HTTP server running")
 })
+
+// Driver nativo
+// ORMs - bibliotecas que tem ferramentas para trabalhar com Databases
+
+// ORM utilizado - Prisma
